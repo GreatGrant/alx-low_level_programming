1c36069b6298680ad3b6aac68c58fe02249d02e9
Grant Williams <greatgrant116@gmail.com>
2022-10-13 07:48:19 -0700

    updated 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 /*
- *Write a C program that prints exactly
- *"Programming is like building a multilingual puzzle" followed by a new line.
- *Use the function puts
- *You are not allowed to use printf
- *Your program should end with the value 0
+ * main - Entry point
+ * Description: "Programming is like building a multilingual puzzle"
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
