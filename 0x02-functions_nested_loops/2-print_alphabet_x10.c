3c5db4f1a44c770b9d53a3eb972d1373e696961a
Grant Williams <greatgrant116@gmail.com>
2022-10-19 07:41:04 -0700

    add 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints 10 times the alphabet,
+ *in lowercase, followed by a new line.
+ *Return: 0 if successful
+ */
+void print_alphabet_x10(void)
+{
+	int counter = 0;
+	char current_letter = 'a';
+	const int LIMIT = 9;
+	const char FINAL_LETTER = 'z';
+
+	while (counter <= LIMIT)
+	{
+		for (current_letter; current_letter <= FINAL_LETTER; current_letter++)
+		{
+			_putchar(current_letter);
+		}
+	}
+
+}
