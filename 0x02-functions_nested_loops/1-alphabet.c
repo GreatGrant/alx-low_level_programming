ed104e0ea98097c860e5334dd2a4fcdc0870bd4e
Grant Williams <greatgrant116@gmail.com>
2022-10-19 07:00:36 -0700

    add  1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints the alphabet, in lowercase, followed by a new line.
+ * Return: 0 if succesful
+ */
+int main(void)
+{
+	char current_letter = 'a';
+
+	while (current_letter <= 'z')
+
+	{
+		_putchar(current_letter)
+		currrent_letter++;
+	}
+	_putchar('\n');
+
+}
