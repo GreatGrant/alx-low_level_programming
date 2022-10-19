cf19fae33185f04398b33839ce869527f9822232
Grant Williams <greatgrant116@gmail.com>
2022-10-19 07:48:43 -0700

    bug fix in 2-alphabet.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,13 @@
 void print_alphabet_x10(void)
 {
 	int counter = 0;
-	char current_letter = 'a';
+	char current_letter;
 	const int LIMIT = 9;
 	const char FINAL_LETTER = 'z';
 
 	while (counter <= LIMIT)
 	{
-		for (current_letter; current_letter <= FINAL_LETTER; current_letter++)
+		for (current_letter = 'a' ; current_letter <= FINAL_LETTER; current_letter++)
 		{
 			_putchar(current_letter);
 		}
