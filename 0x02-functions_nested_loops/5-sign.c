8ed57231d838e1fa5caed2f8f190d3ba46a6783c
Grant Williams <greatgrant116@gmail.com>
2022-10-19 14:50:38 -0700

    add 5-main.c, bug fixed

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_sign - prints the sign of a number.
- * @n is the number
+ * @n:  the number
  * Return: 0 and prints 0 if n is zero
  *or -1 and prints - if n is less than zero
  */
@@ -10,16 +10,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return 1;
+		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('-');
-		return 0;
+		return (0);
 	}
-	else{
+	else
+	{
 		_putchar('-');
-		return -1;
-	
+		return (-1);
+	}
 }
 
