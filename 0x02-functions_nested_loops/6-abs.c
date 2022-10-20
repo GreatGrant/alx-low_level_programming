f5ecf3a8a7f06570b79314e3f9288e9c140d24e5
Grant Williams <greatgrant116@gmail.com>
2022-10-20 04:15:06 -0700

    add  6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_abs - Entry point
+ *Description: computes the absolute value of an integer.
+ *@n: an integer
+ *Return: int
+ **/
+int _abs(int n)
+{
+	if ((n < 0) || (n > 0))
+		return (n);
+	else
+		return (n);
+}
+
